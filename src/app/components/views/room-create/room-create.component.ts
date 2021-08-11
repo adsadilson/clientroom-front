511c4ed79bb413ed6cbe8735a2be24c1bc322f72
adsadilson <adilson.curso@yahoo.com.br>
2021-08-11 02:40:57 -0300

    Personalizado as mensagem de erro

--- src/app/components/views/room-create/room-create.component.ts
+++ src/app/components/views/room-create/room-create.component.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Room } from 'src/app/domain/entity/Room';
@@ -10,9 +11,13 @@ import { Room } from 'src/app/domain/entity/Room';
 })
 export class RoomCreateComponent implements OnInit {
 
-  formRoom!: FormGroup;
+  cadastro!: FormGroup;
 
   constructor(private formBuilder: FormBuilder) { }
+
+  get f() {
+    return this.cadastro.controls;
+  }
 
   ngOnInit(): void {
     this.createForm(new Room());
@@ -20,22 +25,25 @@ export class RoomCreateComponent implements OnInit {
 
 
   createForm(room: Room) {
-    this.formRoom = this.formBuilder.group({
-      name: [room.name, Validators.required, Validators.minLength(4)],
+    this.cadastro = this.formBuilder.group({
+      name: [room.name, Validators.required, Validators.minLength(4), Validators.maxLength(80)],
       date: [room.date, Validators.required],
       startHour: [room.startHour, Validators.required],
       endHour: [room.endHour, Validators.required],
-      active: [room.active]
+      active: [room.active, Validators.required]
     })
   }
 
   onSubmit() {
-    // aqui você pode implementar a logica para fazer seu formulário salvar
-
+    this.cadastro.markAllAsTouched();
+    if (this.cadastro.invalid) {
+      return;
+    }
+    //alert("Registro Salvo com sucesso!\n" + JSON.stringify(this.formRoom.value, null, 4))
   }
 
-  reiniciarForm(){
-
+  reiniciarForm() {
+    this.cadastro.reset();
   }
 
 }
