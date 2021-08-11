b3a27c11fb9d1363ad956268b020061a2dc95ec9
adsadilson <adilson.curso@yahoo.com.br>
2021-08-11 20:04:35 -0300

    Criacao do input number reuso

--- src/app/shared/components/campos/input-number/input-text.component.ts
+++ src/app/shared/components/campos/input-number/input-text.component.ts
@@ -1,24 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { AbstractControl, FormGroup } from '@angular/forms';
 import { ValidarCamposService } from 'src/app/shared/validarCampos/validar-campos.service';
+import { CamposModule } from '../campos.module';
 
 @Component({
-  selector: 'apss-input-number',
-  templateUrl: './input-number.component.html',
-  styleUrls: ['./input-number.component.css']
+  selector: 'apss-input-text',
+  templateUrl: './input-text.component.html',
+  styleUrls: ['./input-text.component.css']
 })
-export class InputNumberComponent {
+export class InputTextComponent {
 
   @Input() formGroup!: FormGroup;
   @Input() titulo!: string;
   @Input() controlName!: string;
-  @Input() minimo = 0;
-  @Input() maximo = 999;
 
   constructor(public validacao: ValidarCamposService) { }
 
   get formControl(): AbstractControl {
     return this.formGroup.controls[this.controlName]
   }
-
 }
